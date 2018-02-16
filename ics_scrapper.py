7b7566994104f78e5a70089c99c100dd1401c195
Victor Haberkorn Gomes <vhaberkorn@gmail.com>
2018-02-16 02:44:12 -0200

    First commit

---
+++ ics_scrapper.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import re
+import requests 
+import os
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+url = "https://isc.sans.edu/podcast.html"
+chat = os.environ.get('TELEGRAM_CHAT_GROUP_ID') 
+updater = Updater(token=os.environ.get('TELEGRAM_BOT_API_TOKEN'))
+dispatcher = updater.dispatcher
+
+def news(bot,update):
+	req = requests.get(url)
+	page_soup = BeautifulSoup(req.text, "html.parser")
+	string = page_soup.blockquote
+	pattern = (r'(<blockquote>\n)(.*?)(<br/> <a href=\")(.*?)(\".*?<br/><br/>)(.*?)(<br/> <a href=\")(.*?)(\".*?<br/><br/>)(.*?)(<br/> <a href=\")(.*?)(\".*?<br/><br/>)')
+	m = re.match(pattern, str(string))
+	msg =  ('1. '+ m.group(2))
+	msg += ('\n    '+ m.group(4))
+	bot.send_message(chat_id=chat, text=msg)
+	msg = ('2. '+ m.group(6))
+	msg += ('\n    '+ m.group(8))
+	bot.send_message(chat_id=chat, text=msg)
+	msg = ('3. '+ m.group(10))
+	msg += ('\n    '+ m.group(12))
+	bot.send_message(chat_id=chat, text=msg)
+
+news_handler = CommandHandler('news', news)
+dispatcher.add_handler(news_handler)
+updater.start_polling()
+
